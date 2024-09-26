b48a495e8ccd88a988fe015457261dff6989cf58
sche34 <timo@scheidel.nl>
2024-09-26 15:22:44 +0200

    use datetime instead of time

--- src/temp_readings/logger.py
+++ src/temp_readings/logger.py
@@ -1,13 +1,13 @@
-import sys
+from datetime import datetime
 import os
 import time
 from vcgencmd import Vcgencmd
 
 def main():
-    start_time = time.time()
     fb = open(f"data/readings.txt","a+")
     if os.stat(f"data/readings.txt").st_size == 0:
-        fb.write("Elapsed Time (s),Temperature (°C),Clock Speed (MHz),Throttled\n")
+        fb.write("Timestamp,Temperature (°C),Clock Speed (MHz),Throttled\n")
+        fb.flush()
 
     vcgm = Vcgencmd()
     try:
@@ -16,17 +16,19 @@ def main():
             clock = int(vcgm.measure_clock('arm')/1000000)
             throttled = vcgm.get_throttled()['breakdown']['2']
 
-            timestamp = round(time.time()) # round to nearest second, so that we can save every X seconds
-            string = '%.0f,%s,%s,%s\n' % (timestamp,temp,clock,throttled)
+            timestamp = datetime.now()
+            string = '%s,%s,%s,%s\n' % (timestamp,temp,clock,throttled)
             fb.write(string)
 
             time.sleep(1)
-            if timestamp % 5 == 0:
+            if timestamp.second % 5 == 0:
                 fb.flush()
+            
+            if timestamp.second % 59 == 0:
+                print(f"Timestamp: {timestamp}, Temp: {temp}, Clock: {clock}, Throttled: {throttled}")
     except KeyboardInterrupt:
         fb.close()
-        print(f"Exiting... at {time.time()}")
-            
+        print(f"Exiting... at {datetime.now()}")
 
 if __name__ == '__main__':
     main()
