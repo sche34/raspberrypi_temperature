8d6d4dbade91211eb44f9c8b7e2cd4758484fc04
sche34 <timo@scheidel.nl>
2024-09-26 15:23:00 +0200

    added a visualization for the readings

---
+++ src/temp_readings/visualize.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main(path="data/readings.txt"):
+    # load & take the average of a minute
+    df = pd.read_csv(path)
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+    grouped_df = df.resample('min', on='Timestamp').mean()
+    grouped_df.reset_index(inplace=True)
+
+    # grouped_df.sort_values(by='Timestamp', inplace=True)
+    grouped_df.plot(x='Timestamp', y='Temperature (°C)', kind='line')
+    
+    # set labels, title and save plot
+    plt.xlabel('Time')
+    plt.ylabel('Temperature (°C)')
+    plt.title('CPU Temperature Readings')
+    plt.savefig('data/temp_readings.png')
+
+    
+
+if __name__ == '__main__':
+    main()
